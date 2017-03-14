7bac11a06e23f85bb356280cbb90399a17def818
Rachith Kumar <rachith1854@gmail.com>
2017-03-14 17:42:10 +0530

    Create script.py

---
+++ Unit 1 - Python Syntax/script.py
@@ -0,0 +1,97 @@
+VARIABLES AND DATA TYPES
+1. Welcome!
+
+# Nothing to do here, just click on "Save and Submit" button to proceeed.
+
+2. Variables
+
+# Write your code below!
+my_variable = 10
+
+3. Booleans
+
+# Set the variables to the values listed in the instructions!
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+4. You've Been Reassigned
+
+# my_int is set to 7 below. What do you think
+# will happen if we reset it to 3 and print the result?
+my_int = 7
+# Change the value of my_int to 3 on line 8!
+my_int = 3 
+
+# Here's some code that will print my_int to the console:
+# The print keyword will be covered in detail soon!
+print my_int
+
+WHITESPACE MEANS RIGHT SPACE
+5. Whitespace
+
+def spam():
+eggs = 12
+return eggs
+print spam()
+
+6. Whitespace Means Right Space
+
+def spam():
+    eggs = 12
+    return eggs
+print spam()
+
+7. A Matter of Interpretation
+
+spam = True
+eggs = False
+
+COMMENTS
+8. Single Line Comments
+
+# this is a single line comment
+
+mysterious_variable = 42
+
+# this is a second line comment
+
+9. Multi-Line Comments
+
+"""this is 
+is a multi-line
+comment
+"""
+
+"""Apollo said
+to
+Zeus
+code in Deus Ex Machina
+"""
+
+MATH OPERATIONS
+10. Math
+
+# Set count_to equal to the sum of two big numbers
+count_to = 100 + 200
+print count_to
+
+11. Exponentiation
+
+#Set eggs equal to 100 using exponentiation on line 3!
+eggs = 10**2 
+print eggs
+
+12. Modulo
+
+#Set spam equal to 1 using modulo on line 3!
+spam = 3 % 2
+print spam
+
+REVIEW
+13. Bringing It All Together 
+
+# this is a single line comment
+monty = True
+python = 1.234
+monty_python = python ** 2
