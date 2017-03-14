abbc20e93cda6ea3ff80b3e66ef732e2e0f3c0bf
Rachith Kumar <rachith1854@gmail.com>
2017-03-14 17:15:12 +0530

    Create script.py

---
+++ Unit 6 - Student Becomes the Teacher/script.py
@@ -0,0 +1,324 @@
+GOOD MORNING CLASS!
+
+1. Lesson Number One
+
+lloyd = {"name": "Lloyd", "homework":[], "quizzes":[], "tests":[],} 
+alice = {"name": "Alice", "homework":[], "quizzes":[], "tests":[],} 
+tyler = {"name": "Tyler", "homework":[], "quizzes":[], "tests":[],}
+
+2. What's the Score?
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+3. Put It Together
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+students = [lloyd, alice ,tyler]
+
+4. For the Record
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+students = [lloyd, alice ,tyler]
+for student in students:
+  print student["name"]
+  print student["homework"]
+  print student["quizzes"]
+  print student["tests"]
+
+JUST AVERAGE
+5. It's Okay to be Average
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+        total = sum(numbers)
+        total = float(total)
+        return float(total)/len(numbers)
+        return total
+    
+6. Just Weight and See
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+        total = sum(numbers)
+        total = float(total)
+        return float(total)/len(numbers)
+        return total
+ 
+def get_average(student):
+   homework=average(student["homework"])
+   quizzes=average(student["quizzes"])
+   tests=average(student["tests"])
+   return (homework*0.1+quizzes*0.3+tests*0.6)
+   
+7. Sending a Letter
+   
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+        total = sum(numbers)
+        total = float(total)
+        return float(total)/len(numbers)
+        return total
+ 
+def get_average(student):
+   homework=average(student["homework"])
+   quizzes=average(student["quizzes"])
+   tests=average(student["tests"])
+   return (homework*0.1+quizzes*0.3+tests*0.6)
+   
+   
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+        
+print get_letter_grade(lloyd)
+
+8. Part of the Whole
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+        total = sum(numbers)
+        total = float(total)
+        return float(total)/len(numbers)
+        return total
+ 
+def get_average(student):
+   homework=average(student["homework"])
+   quizzes=average(student["quizzes"])
+   tests=average(student["tests"])
+   return (homework*0.1+quizzes*0.3+tests*0.6)
+   
+   
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+        
+print get_letter_grade(lloyd)
+
+students = [lloyd,alice,tyler]
+
+def get_class_average(students):
+    results = []
+    for student in students:
+        get_average(student)
+        results.append(get_average(student))
+    return average(results)
+
+9. How is Everybody Doing?
+
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+        total = sum(numbers)
+        total = float(total)
+        return float(total)/len(numbers)
+        return total
+ 
+def get_average(student):
+   homework=average(student["homework"])
+   quizzes=average(student["quizzes"])
+   tests=average(student["tests"])
+   return (homework*0.1+quizzes*0.3+tests*0.6)
+   
+   
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+        
+print get_letter_grade(lloyd)
+
+students = [lloyd,alice,tyler]
+
+def get_class_average(students):
+    results = []
+    for student in students:
+        get_average(student)
+        results.append(get_average(student))
+    return average(results)
+    
+print get_class_average([lloyd, alice, tyler])
+print get_letter_grade(get_class_average([lloyd, alice, tyler]))    
+
+
+
+   
+   
+
+
+
+
+
+
+
+
+
